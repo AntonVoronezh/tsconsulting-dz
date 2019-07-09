d28aae4cc0d8541d3c47a19e8a79eb2ce9b40381
AntonVoronezh <pstut@mail.ru>
2019-07-09 23:23:31 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+	const initialState = { title: '', available: false };
+
+	const [title, setTitle] = useState(initialState);
+
+	return (
+		<div className="container mt-20">
+			<h2>GitHub App for example</h2>
+			<div className="row">
+				<div className="col">
+					<h3>Users</h3>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default App;
